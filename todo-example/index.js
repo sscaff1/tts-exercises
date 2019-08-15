a4a2be7ebc5eb6e16e1b67fead5f7f05fcbda4b1
Steven Scaffidi <sscaff1@gmail.com>
2019-08-15 18:46:45 -0500

    complete todo app

--- todo-example/index.js
+++ todo-example/index.js
@@ -2,7 +2,9 @@
 const formEl = document.querySelector('form');
 const inputEl = formEl.querySelector('#item');
 const listEl = document.querySelector('.list');
+let id = 0;
 let list = [];
+let completedIds = [];
 
 function addItem(e) {
   e.preventDefault();
@@ -10,28 +12,47 @@ function addItem(e) {
   const input = e.target.item;
   const inputValue = input.value;
   const liEl = document.createElement('li');
-  liEl.innerHTML = `${inputValue} <span class="x" data-index="${
-    list.length
-  }">x</span>`;
+  liEl.innerHTML = `<span class="item">${inputValue}</span> <span class="x">x</span>`;
+  id += 1;
+  liEl.dataset.id = id;
   listEl.append(liEl);
-  list.push(inputValue);
+  list.push({ id: id.toString(), value: inputValue });
   input.value = '';
 }
 
-function completeItem() {}
+function completeItem(e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('item')) {
+    const li = e.target.closest('li');
+    const targetId = li.dataset.id;
+    if (completedIds.includes(targetId)) {
+      li.classList.remove('completed');
+      completedIds = completedIds.filter(id => id !== targetId);
+    } else {
+      // added our class complete to the list item
+      li.classList.add('completed');
+      completedIds.push(targetId);
+    }
+  }
+}
 
 function deleteItem(e) {
   e.preventDefault();
 
   if (e.target.classList.contains('x')) {
-    const indexToDelete = parseInt(e.target.dataset.index, 10);
-    list = list.filter((_v, index) => {
-      return index !== indexToDelete;
+    const li = e.target.closest('li');
+    const targetId = li.dataset.id;
+    list = list.filter(v => {
+      return v.id !== targetId;
     });
     listEl.innerHTML = list
-      .map((v, i) => {
+      .map(v => {
+        const liClass = completedIds.includes(v.id) ? 'completed' : '';
         return `
-        <li>${v} <span class="x" data-index="${i}">x</span></li>
+        <li class="${liClass}" data-id=${v.id}><span class="item">${
+          v.value
+        }</span> <span class="x">x</span></li>
       `;
       })
       .join('');
@@ -41,3 +62,4 @@ function deleteItem(e) {
 // event listeners
 formEl.addEventListener('submit', addItem);
 listEl.addEventListener('click', deleteItem);
+listEl.addEventListener('click', completeItem);
