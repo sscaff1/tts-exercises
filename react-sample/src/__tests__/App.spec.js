8b53c820473b1cd47cccf6ace59d2ca22a717062
Steven Scaffidi <sscaff1@gmail.com>
2019-09-24 19:20:11 -0500

    adding tests

---
+++ react-sample/src/__tests__/App.spec.js
@@ -0,0 +1,8 @@
+import App from '../App';
+
+describe('Counter App', () => {
+  it('should render', () => {});
+  it('should increase the count by 1 when increase is pressed', () => {});
+  it('should decrease the count by 1 when the decrease is pressed', () => {});
+  it('should reset back to 0 when reset is pressed', () => {});
+});
